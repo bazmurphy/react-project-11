b19bbbb2d65f39ad12f44ed581096fd2d0e79f9e
Baz Murphy <bazmurphy@gmail.com>
2022-08-12 09:48:30 +0100

    commenting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import './App.css'
 
-import OneApp from "./components/OneApp"
-import TwoApp from "./components/TwoApp"
+// import OneApp from "./components/OneApp"
+// import TwoApp from "./components/TwoApp"
 import ThreeApp from "./components/ThreeApp"
 import FourApp from "./components/FourApp"
 
